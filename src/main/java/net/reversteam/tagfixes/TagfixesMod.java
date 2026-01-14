6512fb0267d40d389168bceef685637596d9c8c0
Mitemi <miter360@gmail.com>
2026-01-14 17:11:23 -0500

    Migrate out of MCR

--- src/main/java/net/reversteam/tagfixes/TagfixesMod.java
+++ src/main/java/net/reversteam/tagfixes/TagfixesMod.java
@@ -32,17 +32,10 @@ public class TagfixesMod {
 	public static final String MODID = "tagfixes";
 
 	public TagfixesMod() {
-		// Start of user code block mod constructor
-		// End of user code block mod constructor
 		MinecraftForge.EVENT_BUS.register(this);
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
-		// Start of user code block mod init
-		// End of user code block mod init
 	}
 
-	// Start of user code block mod methods
-	// End of user code block mod methods
 	private static final String PROTOCOL_VERSION = "1";
 	public static final SimpleChannel PACKET_HANDLER = NetworkRegistry.newSimpleChannel(new ResourceLocation(MODID, MODID), () -> PROTOCOL_VERSION, PROTOCOL_VERSION::equals, PROTOCOL_VERSION::equals);
 	private static int messageID = 0;
