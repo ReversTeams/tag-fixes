de16c3464ab58e13ed451b52db39b792608eaacd
Mitemi <miter360@gmail.com>
2026-01-15 14:13:20 -0500

    Gradle Progress

---
+++ neoforge/src/main/java/net/reversteam/tagfixes/neoforge/TagFixesNeoforge.java
@@ -0,0 +1,13 @@
+package net.reversteam.tagfixes.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import net.reversteam.tagfixes.TagFixes;
+
+@Mod(TagFixes.MOD_ID)
+public final class TagFixesNeoforge {
+    public TagFixesNeoforge() {
+        // Run our common setup.
+        TagFixes.init();
+    }
+}
