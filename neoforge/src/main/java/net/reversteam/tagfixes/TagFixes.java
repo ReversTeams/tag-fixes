dc09dca0430f6a84fb696654f71cf2abb23802ef
Mitemi <miter360@gmail.com>
2026-01-16 14:19:15 -0500

    Test change

--- neoforge/src/main/java/net/reversteam/tagfixes/TagFixes.java
+++ neoforge/src/main/java/net/reversteam/tagfixes/TagFixes.java
@@ -2,12 +2,12 @@ package net.reversteam.tagfixes;
 
 import net.neoforged.fml.common.Mod;
 
-import net.reversteam.tagfixes.TagFixes;
-
 @Mod(TagFixes.MOD_ID)
 public final class TagFixes {
+
+    public static final String MOD_ID = "tagfixes";
+
     public TagFixes() {
-        // Run our common setup.
-        TagFixes.init();
+
     }
 }
